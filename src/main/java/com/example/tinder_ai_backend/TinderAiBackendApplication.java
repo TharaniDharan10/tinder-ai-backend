7d32a9e0de9c6a0c807bbf4ab936cd78ef8e77fc
TharaniDharan10 <TharaniDharan.S@outlook.com>
2024-11-10 00:41:22 +0530

    Add a profile to db and fetch it successfully

--- src/main/java/com/example/tinder_ai_backend/TinderAiBackendApplication.java
+++ src/main/java/com/example/tinder_ai_backend/TinderAiBackendApplication.java
@@ -1,17 +1,40 @@
 package com.example.tinder_ai_backend;
 
+import com.example.tinder_ai_backend.profiles.Gender;
+import com.example.tinder_ai_backend.profiles.Profile;
+import com.example.tinder_ai_backend.profiles.ProfileRepository;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class TinderAiBackendApplication {
+public class TinderAiBackendApplication implements CommandLineRunner{
+
+	@Autowired
+	private ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {	//this is method implemented by CommandLineRunner which runs when the app is up
+		Profile profile = new Profile(
+				"1",
+				"Koushik",
+				"Kothal",
+				40,
+				"Indian",
+				Gender.MALE,
+				"Software programmer",
+				"foo.jpg",
+				"INTP"
+		);
+		profileRepository.save(profile);
+		profileRepository.findAll().forEach(System.out::println);	//now we can see the profile details when we run the app
+	}
 }
 
